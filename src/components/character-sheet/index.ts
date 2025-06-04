96ddef3f66ce182a94fa0efe8c987e510ff15fcf
Rob Garth <rob.garth@aiven.io>
2025-06-05 00:27:11 +1000

    Refactored Equipment Screen

--- src/components/character-sheet/index.ts
+++ src/components/character-sheet/index.ts
@@ -2,4 +2,5 @@
 export { AbilityScores } from './sections/AbilityScores';
 export { HitPointsDisplay } from './sections/HitPointsDisplay';
 export { StatsTab } from './tabs/StatsTab';
-export { ActionsTab } from './tabs/ActionsTab'; 
+export { ActionsTab } from './tabs/ActionsTab';
+export { EquipmentTab } from './tabs/EquipmentTab'; 
