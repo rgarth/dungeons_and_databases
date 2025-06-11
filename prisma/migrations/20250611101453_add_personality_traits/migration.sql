57657acb474a3312940fceabbfb654f4d1c5f012
Rob Garth <rob.garth@aiven.io>
2025-06-11 20:47:12 +1000

    Background data in the DB

---
+++ prisma/migrations/20250611101453_add_personality_traits/migration.sql
@@ -0,0 +1,5 @@
+-- AlterTable
+ALTER TABLE "Character" ADD COLUMN "bonds" JSONB;
+ALTER TABLE "Character" ADD COLUMN "flaws" JSONB;
+ALTER TABLE "Character" ADD COLUMN "ideals" JSONB;
+ALTER TABLE "Character" ADD COLUMN "personalityTraits" JSONB;
